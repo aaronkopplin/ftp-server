3c4cf375ba0cc0075ca60f7518515b4016d52259
Sam Isaacs <isaacss@mail.gvsu.edu>
2020-10-23 19:23:19 -0400

    Added the baseline for transferring files with sockets including the retrieve command

--- ftp_client.py
+++ ftp_client.py
@@ -2,11 +2,48 @@ import socket
 
 LOCALHOST = "localhost"
 PORT = 9999
+FILEPATH = "client_files/"
 
 messenger = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 messenger.connect((LOCALHOST,PORT))
 
-chat = input("message: ")
-messenger.sendall(chat.encode())
+
+def default():
+    print("Command not recognized")
+
+
+def retrieve(filename=None):
+    file = open(FILEPATH+filename, 'w')
+    data = messenger.recv(1024)
+    while data:
+        print(data)
+        file.write(data.decode("utf-8"))
+        data = messenger.recv(1024)
+
+
+while True:
+    chat = input("message: ")
+    if chat:
+        messenger.sendall(chat.encode())
+
+        c = False
+        f = False
+        parsed_message = chat.split(" ")
+
+        if len(parsed_message) >= 1:
+            command = parsed_message[0]
+            c = True
+        if len(parsed_message) >= 2:
+            fname = parsed_message[1]
+            f = True
+
+        if command == "retrieve" and f:
+            retrieve(fname)
+        elif command == "quit":
+            break
+        else:
+            default()
+    else:
+        default()
 
 messenger.close()  # close the socket
--- ftp_server.py
+++ ftp_server.py
@@ -2,21 +2,51 @@ import socket
 
 LOCALHOST = "localhost"
 PORT = 9999
+FILEPATH = "stored_files/"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((LOCALHOST, PORT))
 sock.listen() # wait for someone to start the client script
 messenger, address = sock.accept() # messenger is a socket
 
+
+def default():
+    print("Command not recognized")
+
+
+def retrieve(filename=None):
+    file = open(FILEPATH+filename, 'r')
+    data = file.read(1024)
+    while data:
+        messenger.sendall(data.encode())
+        data = file.read(1024)
+
+
 while True:
     received_message = messenger.recv(1024)
     if received_message:
         print("received message: " + received_message.decode("utf-8"))
 
-    input = input("command: ")
-    if input == "quit":
-        break
+        c = False
+        f = False
+        parsed_message = received_message.decode("utf-8").split(" ")
+        if len(parsed_message) >= 1:
+            command = parsed_message[0]
+            c = True
+        if len(parsed_message) >= 2:
+            fname = parsed_message[1]
+            f = True
+
+        if command == "retrieve" and f:
+            retrieve(fname)
+        elif command == "quit":
+            break
+        else:
+            default()
+    else:
+        default()
 
 messenger.close() # close the socket
+
 
 
